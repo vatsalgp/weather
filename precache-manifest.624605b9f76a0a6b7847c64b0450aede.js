6c37e37ef2adc52cca1d1b0eda06fabcdbe75690
vatsalgp <vatsalgupta280@gmail.com>
2020-02-07 16:04:21 +0530

    Updates

---
+++ precache-manifest.624605b9f76a0a6b7847c64b0450aede.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643af01a5b6beeda0e63ce5806cba200",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "e593db16ecb60af2099b",
+    "url": "/weather/static/css/2.1cad6180.chunk.css"
+  },
+  {
+    "revision": "3166035a853a68920dda",
+    "url": "/weather/static/css/main.e1aa0970.chunk.css"
+  },
+  {
+    "revision": "e593db16ecb60af2099b",
+    "url": "/weather/static/js/2.1cf06f3d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather/static/js/2.1cf06f3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3166035a853a68920dda",
+    "url": "/weather/static/js/main.427a376b.chunk.js"
+  },
+  {
+    "revision": "c99e76bcf2bdae1a8b8a",
+    "url": "/weather/static/js/runtime-main.953fba7f.js"
+  }
+]);
